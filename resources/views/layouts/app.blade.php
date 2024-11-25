a71af11506094bd53b914f3a45281b364e954dc4
mixiii0006 <natasyasalsabilla0905@gmail.com>
2024-11-25 20:03:07 +0700

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -116,6 +116,18 @@
                 @auth
                     @if(Auth::user()->role=='admin'|| Auth::user()->role == 'operator')
                       <li>
+                        <li>
+                            <a
+                              href="{{ route('pemetaan_mk.index') }}"
+                              class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                              >
+                              <svg class="w-6 h-6 text-gray-600 dark:text-gray-400"  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                <path fill-rule="evenodd" d="M11 4.717c-2.286-.58-4.16-.756-7.045-.71A1.99 1.99 0 0 0 2 6v11c0 1.133.934 2.022 2.044 2.007 2.759-.038 4.5.16 6.956.791V4.717Zm2 15.081c2.456-.631 4.198-.829 6.956-.791A2.013 2.013 0 0 0 22 16.999V6a1.99 1.99 0 0 0-1.955-1.993c-2.885-.046-4.76.13-7.045.71v15.081Z" clip-rule="evenodd"/>
+                              </svg>
+
+                              <span class="ml-3">Jadwal Mata Kuliah</span></a
+                            >
+                          </li>
                         <a
                           href="{{ route('data_ruangan.index') }}"
                           class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
@@ -153,18 +165,7 @@
                           <span class="ml-3">Data Dosen</span></a
                         >
                       </li>
-                      <li>
-                        <a
-                          href="{{ route('pemetaan_mk.index') }}"
-                          class="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-                          >
-                          <svg class="w-6 h-6 text-gray-600 dark:text-gray-400"  aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                            <path fill-rule="evenodd" d="M11 4.717c-2.286-.58-4.16-.756-7.045-.71A1.99 1.99 0 0 0 2 6v11c0 1.133.934 2.022 2.044 2.007 2.759-.038 4.5.16 6.956.791V4.717Zm2 15.081c2.456-.631 4.198-.829 6.956-.791A2.013 2.013 0 0 0 22 16.999V6a1.99 1.99 0 0 0-1.955-1.993c-2.885-.046-4.76.13-7.045.71v15.081Z" clip-rule="evenodd"/>
-                          </svg>
 
-                          <span class="ml-3">Jadwal Mata Kuliah</span></a
-                        >
-                      </li>
                     @endif
                 @endauth
                 </ul>
